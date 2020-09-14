db6a4ad386cc805ce5ae229dfd6aadeeb96262ea
saipothanjanjanam <saipothan.janjanam@gmail.com>
2020-09-14 22:36:46 +0530

    Add port in app.js

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.post("/todo", function (req, res) {
 
 
 // Start server on port 3000
-app.listen(3000, function() {
-  console.log("Server has started on port 3000");
+let port = (process.env.PORT || 3000);
+app.listen(port, function() {
+  console.log("Server has started on port " + port);
 })
